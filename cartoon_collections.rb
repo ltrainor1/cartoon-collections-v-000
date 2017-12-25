6be0ba84f70ac1fe0b4b1c55d969299b8e3740dc
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-25 11:15:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  include_ long = false 
+  include_long = false 
   i = 0 
   while i < array.length
     include_long = true if 
