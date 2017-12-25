47c05bc6f8a581e51d5b6e3d6bfba3b79c01a1ec
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-25 11:11:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,9 @@ def long_planeteer_calls(array)
       i += 1 
   end 
 end
+
 long_planeteer_calls([1,2,3,4]) do |x|
-  
+  x.length > 4 
 end 
 def find_the_cheese# code an argument here
   # the array below is here to help
